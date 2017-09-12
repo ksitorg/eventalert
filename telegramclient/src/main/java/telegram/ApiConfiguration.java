00e0f9eec5c842c1636e2ee742f66cf64a2d2826
Tim Wittler <tim@ksit.org>
2017-09-12 17:10:35 +0200

    initial commit

---
+++ telegramclient/src/main/java/telegram/ApiConfiguration.java
@@ -0,0 +1,18 @@
+package telegram;
+
+/**
+ * Created by Tim on 06.09.2017.
+ */
+public class ApiConfiguration { //TODO in property file
+    public static final int API_ID = 0;
+    public static final String API_HASH = "";
+
+    // What you want to appear in the "all sessions" screen
+    public static final String APP_VERSION = "0.0.1-ALPHA";
+    public static final String MODEL = "Windows JVM";
+    public static final String SYSTEM_VERSION = "Java Application";
+    public static final String LANG_CODE = "de";
+
+
+    public static final String TEST_PHONE_NUMBER = ""; // International format
+}
